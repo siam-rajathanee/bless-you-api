63d9a77ca61a6d39f94d9058b55755bc113c6fc9
Maetad Sukarasud <maetad.s@siamraj.com>
2021-07-15 14:06:51 +0700

    fix: cors

--- config/cors.php
+++ config/cors.php
@@ -13,9 +13,9 @@ return [
     |
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
-    */
+     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],
 
     'allowed_methods' => ['*'],
 
